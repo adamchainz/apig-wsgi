c7ab2b1e05a7190e9b0baae8412b66b060be8528
Adam Johnson <me@adamj.eu>
2020-03-13 11:05:29 +0000

    Add HTTP API deployment to example, ref #124

--- example/app/testapp/wsgi.py
+++ example/app/testapp/wsgi.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from apig_wsgi import make_lambda_handler
@@ -7,4 +8,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
 
 application = get_wsgi_application()
 
-lambda_handler = make_lambda_handler(application)
+apig_wsgi_handler = make_lambda_handler(application)
+
+
+def lambda_handler(event, context):
+    print(json.dumps(event, indent=2, sort_keys=True))
+    return apig_wsgi_handler(event, context)
