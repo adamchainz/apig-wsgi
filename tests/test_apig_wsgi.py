81f4a842b072d7c214038136987f3648ea2c6eee
Adam Johnson <me@adamj.eu>
2020-12-04 19:12:04 +0000

    black

--- tests/test_apig_wsgi.py
+++ tests/test_apig_wsgi.py
@@ -75,7 +75,7 @@ def make_v1_event(
     headers_multi=True,
     body="",
     binary=False,
-    request_context=None
+    request_context=None,
 ):
     if headers is None:
         headers = {"Host": ["example.com"]}
@@ -538,7 +538,7 @@ def make_v2_event(
     cookies=None,
     headers=None,
     body="",
-    binary=False
+    binary=False,
 ):
     if cookies is None:
         cookies = []
